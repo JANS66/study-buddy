1f936920c1eb02d9c733c5f213d44a467b7b5e5e
JANS66 <98973687+JANS66@users.noreply.github.com>
2025-12-19 17:49:41 +0200

    Fix API URL for producton deployment

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 // directly (CORS issues + exposing API key). Backend acts as a proxy
 
 export async function getAIResponse(prompt) {
-    const response = await fetch(`http://localhost:3000/api/generate`, {
+    const response = await fetch(`/api/generate`, { // http://localhost:3000/api/generate
         method: `POST`,
         headers: { "Content-Type": "application/json"}, // Tells backend that the request body is JSON
         body: JSON.stringify({ prompt })  // Converts JS object to stirng so it can be sent over HTTP
